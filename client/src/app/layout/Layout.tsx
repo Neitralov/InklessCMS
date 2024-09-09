cc26cbdc62260e38a9fc5545d5b77a91ba9192bb
Maxim Osin <maxim.neitralov@gmail.com>
2024-09-09 17:07:32 +0700

    Добавлена плашка с ссылкой на SwaggerUI (для dev режима)

--- client/src/app/layout/Layout.tsx
+++ client/src/app/layout/Layout.tsx
@@ -13,53 +13,61 @@ export const Layout = () => {
   const logout = useAuthStore((state) => state.logout)
 
   return (
-    <div className={"flex min-h-screen"}>
-      <aside className={"flex flex-col min-w-60 h-screen border-r border-black/20"}>
-        <div className={"flex justify-center py-5 border-b border-black/20"}>
-          <Logo />
+    <>
+      { import.meta.env.DEV &&
+        <div className="flex justify-center bg-green-300">
+          <a href="http://localhost:8080/swagger" target={"_blank"} className="my-1 hover:underline">Открыть Swagger UI</a>
         </div>
+      }
 
-        <nav className={"flex flex-col justify-between h-full px-7 py-5"}>
-          <div className={"flex flex-col gap-5"}>
-            <NavGroup title={"Контент"}>
-              <NavLink
-                to={"/inkless"}
-                className={({ isActive }) => (isActive ? "font-medium " : "") + "flex items-center gap-1.5 hover:font-medium"}>
-                <FileIcon width="20px" height="20px" />
-                Статьи
-              </NavLink>
-              <NavLink
-                end
-                to={"/inkless/collections"}
-                className={({ isActive }) => (isActive ? "font-medium " : "") + "flex items-center gap-1.5 hover:font-medium"}>
-                <CollectionsIcon />
-                Коллекции
-              </NavLink>
-            </NavGroup>
-            <NavGroup title={"API"}>
-              <a
-                href={"http://localhost:8080/swagger"}
-                target={"_blank"}
-                className={"flex items-center gap-1.5 hover:font-medium"}>
-                <ApiIcon />
-                REST API
-              </a>
-            </NavGroup>
+      <div className={"flex min-h-screen"}>
+        <aside className={"flex flex-col min-w-60 h-screen border-r border-black/20"}>
+          <div className={"flex justify-center py-5 border-b border-black/20"}>
+            <Logo />
           </div>
 
-          <div className={"flex flex-col mb-2"}>
-            <div className={"flex flex-col"}>
-              <p
-                className={"flex items-center gap-1.5 hover:font-medium cursor-pointer"}
-                onClick={ logout }>
-                <ExitIcon />
-                Выход
-              </p>
+          <nav className={"flex flex-col justify-between h-full px-7 py-5"}>
+            <div className={"flex flex-col gap-5"}>
+              <NavGroup title={"Контент"}>
+                <NavLink
+                  to={"/inkless"}
+                  className={({ isActive }) => (isActive ? "font-medium " : "") + "flex items-center gap-1.5 hover:font-medium"}>
+                  <FileIcon width="20px" height="20px" />
+                  Статьи
+                </NavLink>
+                <NavLink
+                  end
+                  to={"/inkless/collections"}
+                  className={({ isActive }) => (isActive ? "font-medium " : "") + "flex items-center gap-1.5 hover:font-medium"}>
+                  <CollectionsIcon />
+                  Коллекции
+                </NavLink>
+              </NavGroup>
+              <NavGroup title={"API"}>
+                <a
+                  href={"http://localhost:8080/swagger"}
+                  target={"_blank"}
+                  className={"flex items-center gap-1.5 hover:font-medium"}>
+                  <ApiIcon />
+                  REST API
+                </a>
+              </NavGroup>
             </div>
-          </div>
-        </nav>
-      </aside>
-      <Outlet />
-    </div>
+
+            <div className={"flex flex-col mb-2"}>
+              <div className={"flex flex-col"}>
+                <p
+                  className={"flex items-center gap-1.5 hover:font-medium cursor-pointer"}
+                  onClick={ logout }>
+                  <ExitIcon />
+                  Выход
+                </p>
+              </div>
+            </div>
+          </nav>
+        </aside>
+        <Outlet />
+      </div>
+    </>
   )
 }
