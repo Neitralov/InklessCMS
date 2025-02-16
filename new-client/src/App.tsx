94a000ac5355e720c75f16140f669e8cf3237f53
Maxim Osin <maxim.neitralov@gmail.com>
2025-02-16 20:59:56 +0700

    Init new frontend project

---
+++ new-client/src/App.tsx
@@ -0,0 +1,10 @@
+import { Button } from "./components/ui/button";
+
+export const App = () => {
+  return (
+    <>
+      <h1 className="text-2xl font-bold">Hello, World!</h1>
+      <Button className="cursor-pointer">Click me!</Button>
+    </>
+  );
+};
